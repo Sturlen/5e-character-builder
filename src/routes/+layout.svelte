db5c2d4450eb152f687e364bb5160783b570dd81
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-10-22 17:56:50 +0200

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import './styles.css'
+    import github from '../lib/images/github.svg'
 </script>
 
 <div class="app">
@@ -8,7 +9,10 @@
     </main>
 
     <footer>
-        <a href="https://github.com/Sturlen/5e-character-builder/tree/main">Github Repository</a>
+        <a href="https://github.com/Sturlen/5e-character-builder/tree/main"
+            >Github Repository <img src={github} alt="Github Logo" height="32px" /></a
+        >
+        <a href="https://github.com/Sturlen">Made by Sturlen</a>
     </footer>
 </div>
 
@@ -36,10 +40,14 @@
         justify-content: center;
         align-items: center;
         padding: 12px;
+        background-color: rgba(0, 0, 0, 0.1);
     }
 
     footer a {
         font-weight: bold;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 
     @media (min-width: 480px) {
